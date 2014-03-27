ce0ccd982f5e43968cb867e17374ea24c5d29c92
riccardo <riccardo.to@gmail.com>
2014-03-27 23:05:45 +0100

    aggiunto servizio dichiarativo

--- OSGiService/org.rictomasi.demoservice.core/src/main/java/org/rictomasi/demoservice/core/impl/DemoServiceImpl.java
+++ OSGiService/org.rictomasi.demoservice.core/src/main/java/org/rictomasi/demoservice/core/impl/DemoServiceImpl.java
@@ -4,7 +4,20 @@
 package org.rictomasi.demoservice.core.impl;
 
 import org.rictomasi.demoservice.core.api.DemoService;
+import org.osgi.service.component.ComponentContext; 
 
 public class DemoServiceImpl implements DemoService {
+	
+	
+	protected void activate(ComponentContext context){
+		System.out.println("DemoServiceImpl activate:" + context);
+		
+	}
+
+	protected void deactivate(ComponentContext context){
+		System.out.println("DemoServiceImpl activate:" + context);
+	}
+
+
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 rest-test
 =========
+
+Todo:
+	-add a declarative service
+	-add pax-logging with Slf4j
+	-expose a rest service with GSON
